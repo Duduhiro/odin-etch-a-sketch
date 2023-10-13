5b6e85150b53f4536c6b35d30a23c65de28da59d
Duduhiro <duduhiro0312@gmail.com>
2023-10-13 16:39:40 -0300

    Finished feature to set the numbers of rows

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-function renderSquares() {
+let columns = 0
+
+function setGridSize() {
     const wrapper = document.querySelector('#wrapper')
+    const width = document.getElementById('grid-width').value
+
+    if (width > 16) {
+        alert('Max width 16')
+        return
+    }
+
+    const outer_wrapper = document.querySelector('#outer-wrapper')
+    columns = width
+    wrapper.style.width = (width * 40) + ((width - 1) * 10) + 'px;'
+    renderSquares()
+    wrapper.remove()
+}
+
+function renderSquares() {
+    const wrapper = document.createElement('div')
+    wrapper.setAttribute('id', 'wrapper')
+    const outer_wrapper = document.querySelector('#outer-wrapper')
     const reset = document.getElementById('reset')
-    for (let i = 0; i < 256; i++) {
+
+    for (let i = 0; i < (16 * columns); i++) {
         const div = document.createElement('div')
         div.addEventListener('mouseover', () => {
             div.style.backgroundColor = 'red'
@@ -11,6 +32,5 @@ function renderSquares() {
         })
         wrapper.appendChild(div)
     }
+    outer_wrapper.append(wrapper)
 }
-
-renderSquares()
