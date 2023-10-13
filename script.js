f38fc81f7ade78cb0fd442437e354a2d9bbcc1e5
Duduhiro <duduhiro0312@gmail.com>
2023-10-13 02:46:56 -0300

    Made the brush function and the reset button

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 function renderSquares() {
     const wrapper = document.querySelector('#wrapper')
-    
+    const reset = document.getElementById('reset')
     for (let i = 0; i < 256; i++) {
         const div = document.createElement('div')
-        
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = 'red'
+            reset.addEventListener('click', () => {
+                div.style.backgroundColor = 'burlywood'
+            })
+        })
         wrapper.appendChild(div)
     }
 }
